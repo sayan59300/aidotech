f0e5bbc3fe0d55c8edce9f43d58a4947e3e1ef5e
Sayan593 <concepteur-developpeur@nicolas-buffart.fr>
2017-01-23 02:27:20 +0100

    init project

---
+++ src/Aidotech/CoreBundle/AidotechCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aidotech\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AidotechCoreBundle extends Bundle
+{
+}
---
+++ src/Aidotech/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aidotech\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AidotechCoreBundle:Default:index.html.twig');
+    }
+}
