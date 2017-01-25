28a043f88d7792dc7c8b59fddabe6cf3b449f19b
Sayan593 <concepteur-developpeur@nicolas-buffart.fr>
2017-01-25 20:34:15 +0100

    ajout page mentions légales

--- src/Aidotech/CoreBundle/Controller/DefaultController.php
+++ src/Aidotech/CoreBundle/Controller/DefaultController.php
@@ -15,4 +15,9 @@ class DefaultController extends Controller
     {
         return $this->render('AidotechCoreBundle:Default:contact.html.twig');
     }
+    
+    public function mentionsAction()
+    {
+        return $this->render('AidotechCoreBundle:Default:mentions-legales.html.twig');
+    }
 }
