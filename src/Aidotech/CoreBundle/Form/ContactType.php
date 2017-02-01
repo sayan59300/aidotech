c146dbbd4ab4db392c6f9fcd8fe730cce0777aef
Sayan593 <concepteur-developpeur@nicolas-buffart.fr>
2017-02-01 10:37:56 +0100

    rectification taille textarea contact

--- src/Aidotech/CoreBundle/Form/ContactType.php
+++ src/Aidotech/CoreBundle/Form/ContactType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ContactType extends AbstractType {
-  
+
   /**
    * {@inheritdoc}
    */
@@ -19,7 +19,8 @@ class ContactType extends AbstractType {
         ->add('prenom', TextType::class, ['label' => 'Votre prénom*'])
         ->add('email', EmailType::class, ['label' => 'Votre email*'])
         ->add('objet', TextType::class, ['label' => 'Objet du message*'])
-        ->add('message', TextareaType::class, ['label' => 'Votre message*']);
+        ->add('message', TextareaType::class, ['label' => 'Votre message*',
+            'attr' => array('rows' => '10')]);
   }
-  
+
 }
